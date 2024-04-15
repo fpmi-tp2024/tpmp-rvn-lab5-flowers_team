eb5bcd118e9754039627edde909be6ed1b5ec8d8
VVendor <vadprof10@gmail.com>
2024-04-15 14:42:21 +0300

    add registration

--- include/User.hpp
+++ include/User.hpp
@@ -1,14 +1,31 @@
 #pragma once
 #include <string>
 
-enum class User_Role{Simple_User, Admin};
+enum class User_Role
+{
+    Simple_User,
+    Admin
+};
 
-class User{
+class User
+{
 
-    private:
+private:
     int user_id;
     std::string login;
     std::string hash_password;
     User_Role role;
-    
+
+public:
+    User() = default;
+    User(std::string _login, std::string _hash_password, User_Role _role = User_Role::Simple_User) : login(_login), hash_password(_hash_password), role(_role) {}
+    void SetUserId(int id);
+    void SetUserLogin(std::string _login);
+    void SetUserHashPassword(std::string _hash_password);
+    void SetUserRole(User_Role _role);
+    int GetUserId() const;
+    std::string GetLogin() const;
+    std::string GetHashPassword() const;
+    User_Role GetUserRole() const;
+    ~User() = default;
 };
