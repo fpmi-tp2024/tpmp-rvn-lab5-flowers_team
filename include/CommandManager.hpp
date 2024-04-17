5102700e64efdcd2cacad485772902665ba308d7
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2024-04-17 11:07:27 +0300

    add command manager menu

--- include/CommandManager.hpp
+++ include/CommandManager.hpp
@@ -1,1 +1,21 @@
 #pragma once
+
+#include <map>
+#include <memory>
+
+#include "../include/Authorization.hpp"
+#include "../include/Registration.hpp"
+class CommandManager {
+
+    private:
+    std::map<int,std::shared_ptr<AccessCommand>>access_command_table;
+    SQLite::Database db;
+    void InitAccessCommands();
+    void PrintAccessCommands();
+    
+    
+    public:
+    CommandManager(std::string db_path);
+    ~CommandManager()=default;
+    void Start();
+};
