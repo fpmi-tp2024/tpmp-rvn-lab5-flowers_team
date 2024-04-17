9ef0156a5b59fa56d8d213cdee5265c322780a9b
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2024-04-17 09:03:08 +0300

    add authorization

--- include/Authorization.hpp
+++ include/Authorization.hpp
@@ -6,6 +6,7 @@ class Authorization : public AccessCommand
 {
 public:
     Authorization();
+    bool FindUser(SQLite::Database &db, User &user);
     User execute(SQLite::Database &db) override;
     ~Authorization() = default;
 };
