3b012ee37b193d38042d72913fcbece9b9794a31
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2024-04-22 14:41:48 +0300

    update composition models

--- src/Composition.cpp
+++ src/Composition.cpp
@@ -4,7 +4,7 @@ void Composition::SetIdComposition(int _id_composition)
 {
     id_composition = _id_composition;
 }
-int Composition::GetIdComposition()const
+int Composition::GetIdComposition() const
 {
     return id_composition;
 }
@@ -25,27 +25,32 @@ int Composition::GetCompositionCost() const
     return composition_cost;
 }
 
-void Composition::AddFlower(int _id_flower, std::string _flower_name, std::string _variety, int _flower_cost, int flower_count) 
+void Composition::AddFlower(int _id_flower, std::string _flower_name, std::string _variety, int _flower_cost, int flower_count)
 {
-    flowers.emplace_back(Flower(_id_flower,_flower_name, _variety,_flower_cost), flower_count);
+    flowers.emplace_back(Flower(_id_flower, _flower_name, _variety, _flower_cost), flower_count);
 }
 
-std::string Composition::toString() 
+std::string Composition::toString()
 {
+    std::cout << "Composition information" << std::endl;
+    std::cout << "Composition id = " << GetIdComposition() << std::endl;
+    std::cout << "Composition name: " << GetCompositionName() << std::endl;
+    std::cout << "Composition cost: " << GetCompositionCost() << std::endl;
+    std::cout << std::endl;
+    std::cout << "All information about the flowers included in the composition" << std::endl;
     std::string result;
-    for (const auto& pair : flowers) {
+    for (const auto &pair : flowers)
+    {
         result += "Flower id = " + std::to_string(pair.first.GetIdFlower()) + "\n";
         result += "Flower name: " + pair.first.GetFlowerName() + "\n";
         result += "Flower variety: " + pair.first.GetVariety() + "\n";
         result += "Flower cost: " + std::to_string(pair.first.GetFlowerCost()) + "\n";
         result += "Flower count: " + std::to_string(pair.second) + "\n";
-         result += "\n";
+        result += "\n";
     }
     return result;
 }
-Composition:: ~Composition()
+Composition::~Composition()
 {
     flowers.clear();
 }
-
-    
