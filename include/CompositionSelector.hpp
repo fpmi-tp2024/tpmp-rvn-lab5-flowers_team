95ffb23fc372933be8857f573cbb1ce28da0f61a
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2024-04-22 14:40:46 +0300

    "update composition selector"

--- include/CompositionSelector.hpp
+++ include/CompositionSelector.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "../include/Command.hpp"
+#include "../include/Composition.hpp"
 
 class CompositionSelector: public Command
 {
     public:
     CompositionSelector();
     void execute(SQLite::Database &db , std::optional<User>user_info = std::nullopt) override;
+    bool GetComposition(Composition &composition, SQLite::Database &db);
+    bool GetFlowerComposition(Composition &composition, SQLite::Database &db);
     ~CompositionSelector() =default;
     
 };
--- src/CompositionSelector.cpp
+++ src/CompositionSelector.cpp
@@ -1,18 +1,11 @@
 #include "../include/CompositionSelector.hpp"
-#include "../include/Composition.hpp"
 
 #include <iostream>
-CompositionSelector::CompositionSelector(): Command("Get info about composition\n"){}
-void CompositionSelector::execute(SQLite::Database &db, std::optional<User> user_info)
+CompositionSelector::CompositionSelector() : Command("Get info about composition\n") {}
+bool CompositionSelector::GetComposition(Composition &composition, SQLite::Database &db)
 {
-    std::string composition_name;
-    Flower flower{};
-    Composition composition{};
-    std::cout << "Enter composition name: " << std::endl;
-    std::getline(std::cin, composition_name);
-
     SQLite::Statement query(db, "SELECT id, composition_name, composition_cost FROM Composition WHERE composition_name = ?;");
-    query.bind(1, composition_name);
+    query.bind(1, composition.GetCompositionName());
     bool is_composition_find = false;
     try
     {
@@ -21,46 +14,59 @@ void CompositionSelector::execute(SQLite::Database &db, std::optional<User> user
             composition.SetIdComposition(query.getColumn(0).getInt());
             composition.SetCompositionName(query.getColumn(1).getString());
             composition.SetCompositionCost(query.getColumn(2).getInt());
+            is_composition_find = true;
         }
-        is_composition_find = true;
     }
     catch (const std::exception &e)
     {
-        std::cout << "No composition witn name " << composition_name << std::endl;
+        std::cout << e.what() << std::endl;
     }
-
     if (!is_composition_find)
-        return;
-
-    SQLite::Statement query_flower(db, "SELECT Flower.id, Flower.flower_name, Flower.variety, Flower.flower_cost, Composition_flower.flower_count FROM Composition_flower INNER JOIN Flower ON Composition_flower.id_flower = Flower.id WHERE Composition_flower.composition_id = ?;");
-    query_flower.bind(1, composition.GetIdComposition());
+    {
+        std::cout << "No composition with name " << composition.GetCompositionName() << std::endl;
+    }
+    return is_composition_find;
+}
+bool CompositionSelector::GetFlowerComposition(Composition &composition, SQLite::Database &db)
+{
+    Flower flower{};
+    SQLite::Statement query(db, "SELECT Flower.id, Flower.flower_name, Flower.variety, Flower.flower_cost, Composition_flower.flower_count FROM Composition_flower INNER JOIN Flower ON Composition_flower.id_flower = Flower.id WHERE Composition_flower.composition_id = ?;");
+    query.bind(1, composition.GetIdComposition());
     bool is_flower_composition_find = false;
     try
     {
-        while (query_flower.executeStep())
+        while (query.executeStep())
         {
-            flower.SetIdFlower(query_flower.getColumn(0).getInt());
-            flower.SetFlowerName(query_flower.getColumn(1).getString());
-            flower.SetVariety(query_flower.getColumn(2).getString());
-            flower.SetFlowerCost(query_flower.getColumn(3).getInt());
-            composition.AddFlower(flower.GetIdFlower(), flower.GetFlowerName(), flower.GetVariety(), flower.GetFlowerCost(), query_flower.getColumn(4).getInt());
+            flower.SetIdFlower(query.getColumn(0).getInt());
+            flower.SetFlowerName(query.getColumn(1).getString());
+            flower.SetVariety(query.getColumn(2).getString());
+            flower.SetFlowerCost(query.getColumn(3).getInt());
+            composition.AddFlower(flower.GetIdFlower(), flower.GetFlowerName(), flower.GetVariety(), flower.GetFlowerCost(), query.getColumn(4).getInt());
+            is_flower_composition_find = true;
         }
-        is_flower_composition_find = true;
     }
     catch (const std::exception &e)
     {
-        std::cout << "No information! " << std::endl;
+        std::cout << e.what() << std::endl;
     }
-
     if (!is_flower_composition_find)
-        return;
+    {
+        std::cout << "No information!" << std::endl;
+    }
+    return is_flower_composition_find;
+}
+void CompositionSelector::execute(SQLite::Database &db, std::optional<User> user_info)
+{
+    std::string composition_name;
+    Composition composition{};
+    std::cout << "Enter composition name: " << std::endl;
+    std::getline(std::cin, composition_name);
+    std::cout << std::endl;
+    composition.SetCompositionName(composition_name);
 
-    std::cout<<"Composition information"<<std::endl;
-    std::cout << "Composition id = " << composition.GetIdComposition() << std::endl;
-    std::cout << "Composition name: " << composition.GetCompositionName() << std::endl;
-    std::cout<< "Composition cost: "<< composition.GetCompositionCost() << std::endl;
-    std::cout<<std::endl;
-    std::cout<<"All information about the colors included in the composition"<<std::endl;
-    std::cout<< composition.toString()<<std::endl;
-
+    if (GetComposition(composition, db))
+    {
+        GetFlowerComposition(composition, db);
+        std::cout << composition.toString() << std::endl;
+    }
 }
