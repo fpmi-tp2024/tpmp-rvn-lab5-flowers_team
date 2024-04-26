71a648f8c0f53c24c2830fb6987f55e7910e0a4b
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2024-04-26 09:20:59 +0300

    add tests for registration

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "../include/Validator.hpp"
+#include "../include/Registration.hpp"
 
 TEST(ValidationTest, ValidPassword)
 {
@@ -19,6 +20,26 @@ TEST(ValidationTest, InvalidLogin)
 {
     ASSERT_FALSE(Validator::ValidateLogin("4567"));
     ASSERT_FALSE(Validator::ValidateLogin("HU@hhk"));
+}
+TEST(RegistrationTest, AddUser)
+{
+    SQLite::Database db("../db/flower.db", SQLite::OPEN_READWRITE);
+    std::hash<std::string> hasher;
+    std::string hash_password = std::to_string(hasher("wqr8t321"));
+    std::string login = "valya234";
+    Registration registration;
+    User user{login, hash_password};
+    ASSERT_TRUE(registration.AddUser(db,user));
+}
+TEST(RegistrationTest, AddExistingUser)
+{
+    SQLite::Database db("../db/flower.db", SQLite::OPEN_READWRITE);
+    std::hash<std::string> hasher;
+    std::string hash_password = std::to_string(hasher("12345678"));
+    std::string login = "hello23";
+    Registration registration;
+    User user{login, hash_password};
+    ASSERT_FALSE(registration.AddUser(db,user));
 }
 
 int main(int argc, char **argv)
