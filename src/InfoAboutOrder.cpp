767bf3ebeec5e4216da36df737754b22efd53c7b
VVendor <vadprof10@gmail.com>
2024-05-12 23:40:14 +0300

    refactoring InfoAboutOrder

--- src/InfoAboutOrder.cpp
+++ src/InfoAboutOrder.cpp
@@ -1,5 +1,5 @@
 #include <string>
-#include <iostream>
+#include <vector>
 #include "../include/InfoAboutOrder.hpp"
 #include "../include/Order.hpp"
 
@@ -8,48 +8,49 @@ InfoAboutOrder::InfoAboutOrder() : Command("All information about the order") {}
 void InfoAboutOrder::execute(SQLite::Database &db, std::optional<User> user_info)
 {
     std::string date;
-    Order order{};
+    std::vector<Order> orders_info;
     std::cout << "Enter the date to receive information on all orders" << std::endl;
     std::getline(std::cin, date);
 
     SQLite::Statement query(db, "SELECT id, composition_count, order_date, release_date, user_id, composition_id \
     FROM \"Order\" WHERE order_date = ?;");
     query.bind(1, date);
-    bool is_order_find = false;
     try
     {
         while (query.executeStep())
         {
-            order.SetIdOrder(query.getColumn(0).getInt());
-            order.SetCompositionCount(query.getColumn(1).getInt());
-            order.SetOrderDate(query.getColumn(2).getString());
-            order.SetReleaseDate(query.getColumn(3).getString());
-            order.SetUserId(query.getColumn(4).getInt());
-            order.SetCompositionId(query.getColumn(5).getInt());
+            int order_id = query.getColumn(0).getInt();
+            int composition_count = query.getColumn(1).getInt();
+            std::string order_date = query.getColumn(2).getString();
+            std::string release_date = query.getColumn(3).getString();
+            int user_id = query.getColumn(4).getInt();
+            int composition_id = query.getColumn(5).getInt();
 
-            SQLite::Statement query1(db, "SELECT cost FROM Order_Cost WHERE id = ?;");
-            query1.bind(1, order.GetIdOrder());
-            while (query1.executeStep())
+            SQLite::Statement cost_query(db, "SELECT cost FROM Order_Cost WHERE id = ?;");
+            cost_query.bind(1, order_id);
+            while (cost_query.executeStep())
             {
-                order.SetCost(query1.getColumn(0).getInt());
+                int cost = cost_query.getColumn(0).getInt();
+                orders_info.emplace_back(order_id, composition_id, user_id, order_date, release_date,
+                                         composition_count, cost);
             }
-            order.AddOrder(order.GetIdOrder(), order.GetCompositionId(), order.GetUserId(), order.GetOrderDate(),
-                           order.GetReleaseDate(), order.GetCompositionCount(), order.GetCost());
-            is_order_find = true;
         }
     }
     catch (const std::exception &e)
     {
         std::cout << e.what() << std::endl;
     }
-    if (!is_order_find)
+    if (orders_info.empty())
     {
         std::cout << "No information available for the specified date" << std::endl;
     }
     else
     {
         std::cout << "All information about orders by date: " << date << std::endl;
-        std::cout << order.toString() << std::endl;
+        for (const auto &order : orders_info)
+        {
+            std::cout << order << std::endl;
+        }
     }
 }
 
