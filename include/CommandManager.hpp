df23de72a1ec1b64811d951dc4af26905e627d0f
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2024-05-15 09:27:33 +0300

    update command manager

--- include/CommandManager.hpp
+++ include/CommandManager.hpp
@@ -5,12 +5,25 @@
 
 #include "../include/Authorization.hpp"
 #include "../include/Registration.hpp"
+#include "../include/CompositionSelector.hpp"
+#include "../include/CompositionProfitAnalyzer.hpp"
+#include "../include/CompositionMaxDemandAnalyzer.hpp"
+#include "../include/FlowerVarietyPeriodAnalyzer.hpp"
+#include "../include/FlowerCostUpdate.hpp"
+#include "../include/OrderPeriodCountAnalyzer.hpp"
+#include "../include/OrderPeriodProfitAnalyzer.hpp"
+#include "../include/InfoAboutOrder.hpp"
+#include "../include/NewOrderCostCalculator.hpp"
+
 class CommandManager
 {
 private:
     void InitAccessCommands();
-    void PrintAccessCommands();
-
+    void PrintAccessCommands() const;
+    void InitCommandsSimpleUser();
+    void InitCommandsAdmin();
+    void PrintCommands(const std::map<int, std::shared_ptr<Command>>& commands) const;
+    void CommandsWorker(const std::map<int, std::shared_ptr<Command>>& commands, const User &user);
 public:
     CommandManager(std::string db_path);
     ~CommandManager();
@@ -18,5 +31,7 @@ public:
 
 private:
     std::map<int, std::shared_ptr<AccessCommand>> access_command_table;
+    std::map<int, std::shared_ptr<Command>> simple_user_command_table;
+    std::map<int, std::shared_ptr<Command>> admin_command_table;
     SQLite::Database db;
 };
--- src/CommandManager.cpp
+++ src/CommandManager.cpp
@@ -4,6 +4,8 @@
 CommandManager::CommandManager(std::string db_path) : db(db_path, SQLite::OPEN_READWRITE)
 {
     InitAccessCommands();
+    InitCommandsSimpleUser();
+    InitCommandsAdmin();
 }
 
 void CommandManager::InitAccessCommands()
@@ -13,6 +15,25 @@ void CommandManager::InitAccessCommands()
         {2, std::make_shared<Registration>()}};
 }
 
+void CommandManager::InitCommandsSimpleUser()
+{
+    simple_user_command_table = {
+        {1, std::make_shared<CompositionMaxDemandAnalyzer>()},
+        {2, std::make_shared<InfoAboutOrder>()},
+        {3, std::make_shared<NewOrderCostCalculator>()}};
+}
+void CommandManager::InitCommandsAdmin()
+{
+    admin_command_table = {
+        {1, std::make_shared<CompositionSelector>()},
+        {2, std::make_shared<CompositionProfitAnalyzer>()},
+        {3, std::make_shared<CompositionMaxDemandAnalyzer>()},
+        {4, std::make_shared<InfoAboutOrder>()},
+        {5, std::make_shared<NewOrderCostCalculator>()},
+        {6, std::make_shared<OrderPeriodCountAnalyzer>()},
+        {7, std::make_shared<OrderPeriodProfitAnalyzer>()},
+        {8, std::make_shared<FlowerCostUpdate>()}};
+}
 void CommandManager::Start()
 {
     int op_id = -1;
@@ -34,13 +55,19 @@ void CommandManager::Start()
             break;
         }
     }
-
     if (!op_id)
         return;
-    // to do : menu for admin or menu for user according to role
+    if (user.GetUserRole() == User_Role::Admin)
+    {
+        CommandsWorker(admin_command_table, user);
+    }
+    else
+    {
+        CommandsWorker(simple_user_command_table, user);
+    }
 }
 
-void CommandManager::PrintAccessCommands()
+void CommandManager::PrintAccessCommands() const
 {
     std::cout << "Access menu: " << std::endl;
     std::cout << "0. Exit" << std::endl;
@@ -50,4 +77,34 @@ void CommandManager::PrintAccessCommands()
     }
 }
 
+void CommandManager::PrintCommands(const std::map<int, std::shared_ptr<Command>> &commands) const
+{
+    std::cout << "Commands menu: " << std::endl;
+    std::cout << "0. Exit" << std::endl;
+    for (const auto &command : commands)
+    {
+        std::cout << command.first << ". " << command.second->GetDescription() << std::endl;
+    }
+}
+
+void CommandManager::CommandsWorker(const std::map<int, std::shared_ptr<Command>> &commands, const User &user)
+{
+    int op_id = -1;
+    while (true)
+    {
+        PrintCommands(commands);
+        std::cin >> op_id;
+        std::cin.get();
+        if (!op_id)
+            break;
+        if (commands.find(op_id) == commands.end())
+        {
+            std::cout << "Invalid command. Please, try again!" << std::endl;
+        }
+        else
+        {
+            commands.at(op_id)->execute(db, user);
+        }
+    }
+}
 CommandManager::~CommandManager() = default;
